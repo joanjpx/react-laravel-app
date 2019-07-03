6407f9fe044a128b9ff9c87359c4bd28319ee674
Joan P. Alvarado <joansyn.s@gmail.com>
2019-07-02 23:17:04 -0500

    Creating Component Tweet

--- react-api/resources/views/welcome.blade.php
+++ react-api/resources/views/welcome.blade.php
@@ -15,17 +15,7 @@
         <div id="example"></div>
         <div id="tweet-component"></div>
         <div class="container" id="tweets">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                <div class="media text-muted pt-3">
-                    <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-                    <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                        <strong class="d-block text-gray-dark">@username</strong>
-                        Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-                    </p>
-                </div>
-                </div>
-            </div>
+            
         </div>
         
     </body>
